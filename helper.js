a09dffd5a75dfbd860765d2ad3e73ca80d3bb3af
ANKIT PRATAP SINGH <jadounankit17@gmail.com>
2024-06-11 07:21:57 +0530

    few changes

--- helper.js
+++ helper.js
@@ -1,9 +1,58 @@
+// let data = [
+//     { title: 'Lead Data', link: './Lead Data/1.~$noida.xlsx' },
+//     { title: '2.car-loan-database-in-delhi', link: './Lead Data/2.car-loan-database-in-delhi.xlsx' },
+//     { title: '3.cold-chain-logistics-in-delhi', link: './Lead Data/3.cold-chain-logistics-in-delhi.xlsx' },
+//     { title: '3.cold-chain-logistics-in-delhi', link: './Lead Data/4.cold' },
+// ];
+
+// data.forEach(element => {
+//     document.write(`<tr><td>${element.title}</td><td><a href='${element.link}'>Download</a></td></tr>`);
+// });
+
+
+
 let data = [
     { title: 'Lead Data', link: './Lead Data/1.~$noida.xlsx' },
     { title: '2.car-loan-database-in-delhi', link: './Lead Data/2.car-loan-database-in-delhi.xlsx' },
     { title: '3.cold-chain-logistics-in-delhi', link: './Lead Data/3.cold-chain-logistics-in-delhi.xlsx' },
+    { title: '4.cold-chain-logistics-in-delhi-ncr', link: './Lead Data/4.cold-chain-logistics-in-delhi-ncr.xlsx' },
+    { title: '5.corrugated-box-manufacturing-company-in-delhi', link: './Lead Data/5.corrugated-box-manufacturing-company-in-delhi.xlsx' },
+    { title: '6.delhi-state-data', link: './Lead Data/6.delhi-state-data.xlsx' },
+    { title: '7.dentist-in-delhi', link: './Lead Data/7.dentist-in-delhi.xlsx' },
+    { title: '8.docotors-in-delhi', link: './Lead Data/8.docotors-in-delhi.xlsx' },
+    { title: '9.email-ids-of-doctors-in-delhi', link: './Lead Data/9.email-ids-of-doctors-in-delhi.xlsx' },
+    { title: '10. HIGH INC', link: './Lead Data/10. HIGH INC.xls' },
+    { title: '11.property-seekers-in-delhi-ncr', link: './Lead Data/11.property-seekers-in-delhi-ncr.xlsx' },
+    { title: '12.salaried-person-in-delhi-ncr', link: './Lead Data/12.salaried-person-in-delhi-ncr.xlsx' },
+    { title: '13..calling-candidate-data-in-noida', link: './Lead Data/13..calling-candidate-data-in-noida.xlsx' },
+    { title: '14.contact-details-database-in-noida', link: './Lead Data/14.contact-details-database-in-noida.xlsx' },
+    { title: '15.corrugated-box-manufacturing-company-in-noida', link: './Lead Data/15.corrugated-box-manufacturing-company-in-noida.xlsx' },
+    { title: '16.dentist-in-noida', link: './Lead Data/16.dentist-in-noida.xlsx' },
+    { title: '17.hni-database-in-noida', link: './Lead Data/17.hni-database-in-noida.xlsx' },
+    { title: '18.home-loan-agency-in-noida', link: './Lead Data/18.home-loan-agency-in-noida.xlsx' },
+    { title: '19.noida-city-data', link: './Lead Data/19.noida-city-data.xlsx' },
+    { title: '20.property-holder-database-in-noida-and-delhi', link: './Lead Data/20.property-holder-database-in-noida-and-delhi.xlsx' },
+    { title: '21.tutor-data-in-noida', link: './Lead Data/21.tutor-data-in-noida.xlsx' },
+    { title: '22.advertising ,pr & events in delhi 110085', link: './Lead Data/22.advertising ,pr & events in delhi 110085.xlsx' },
+    { title: '23.Advertising, PR & Events---3136', link: './Lead Data/23.Advertising, PR & Events---3136.xlsx' },
+    { title: '24.Advertising, PR & Events-25300', link: './Lead Data/24.Advertising, PR & Events-25300.xlsx' },
+    { title: '25.Apparel  Garments And  Gems & Jewellery-15253', link: './Lead Data/25.Apparel  Garments And  Gems & Jewellery-15253.xlsx' },
+    { title: '26.apparel Garaments And Gems & Jewellery in delhi 110085', link: './Lead Data/26.apparel Garaments And Gems & Jewellery in delhi 110085.xlsx' },
+    { title: '27.Apparel Garments , Gems & Jewellery--3656', link: './Lead Data/27.Apparel Garments , Gems & Jewellery--3656.xlsx' },
+    { title: '28.automobiles database in 110085', link: './Lead Data/28.automobiles database in 110085.xlsx' },
+    { title: '29.Automobiles--7591', link: './Lead Data/29.Automobiles--7591.xlsx' },
+    { title: '30.Automobiles-18273', link: './Lead Data/30.Automobiles-18273.xlsx' },
+    { title: '31.bank insurance data base in delhi 110085', link: './Lead Data/31.bank insurance data base in delhi 110085.xlsx' },
+    { title: '32.banking database in delhi 110085', link: './Lead Data/32.banking database in delhi 110085.xlsx' },
+    { title: '33.Banking-11816', link: './Lead Data/33.Banking-11816.xlsx' },
+    { title: '34.Banks  Insurance & Financial Services--9291', link: './Lead Data/34.Banks  Insurance & Financial Services--9291.xlsx' },
+    { title: '35.Banks, Insurance & Financial Services-65000', link: './Lead Data/35.Banks, Insurance & Financial Services-65000.xlsx' },
+    { title: '36.BPO, CRM & Call Centres  --15810---', link: './Lead Data/36.BPO, CRM & Call Centres  --15810---.xlsx' },
+
+
+
 ];
 
 data.forEach(element => {
-    document.write(`<tr><td>${element.title}</td><td><a href='${element.link}'>Download</a></td></tr>`);
+    document.write(<tr><td>${element.title}</td><td><a href='${element.link}'>Download</a></td></tr>);
 });
